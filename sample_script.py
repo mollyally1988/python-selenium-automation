8d4c6d7c2e61fb15b1be9843723b147da8213670
Alina Dorofeyeva <alinadorofeyeva@Alinas-MBP.home>
2025-03-08 21:48:45 -0800

    Update search script

--- sample_script.py
+++ sample_script.py
@@ -13,21 +13,19 @@ driver = webdriver.Chrome(service=service)
 driver.maximize_window()
 
 # open the url
-driver.get('https://www.google.com/')
+driver.get("https://www.target.com/")
 
-# populate search field
-search = driver.find_element(By.NAME, 'q')
-search.clear()
-search.send_keys('Table')
+#Click SignIn button
+driver.find_element(By.XPATH, "//a[@id='account']")
 
-# wait for 4 sec
-sleep(4)
+#Click SignIn from side navigation
+driver.find_element(By.XPATH, "//a[contains(text(),'Sign in')]")
 
-# click search button
-driver.find_element(By.NAME, 'btnK').click()
+#Verify SignIn page opened:
+driver.find_element(By.XPATH, "//h1[text()='Sign into your Target account']")
 
-# verify search results
-assert 'table'.lower() in driver.current_url.lower(), f"Expected query not in {driver.current_url.lower()}"
-print('Test Passed')
+#SignIn button is shown (you can just use driver.find_element() to check for element’s presence, no need to assert here)
+driver.find_element(By.XPATH,"//button[contains(@type,'submit') and contains(text(),'Sign in')]")
+
 
 driver.quit()
